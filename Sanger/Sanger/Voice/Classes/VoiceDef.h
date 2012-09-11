6862cfcc9e93372f49b107733f6a622d657ca5b4
sh00008 <sh00008@sohu.com>
2012-09-11 18:00:52 +0800

    添加Voice相关的文件。

---
+++ Sanger/Sanger/Voice/Classes/VoiceDef.h
@@ -0,0 +1,14 @@
+//
+//  VoiceDef.h
+//  Sanger
+//
+//  Created by JiaLi on 12-8-16.
+//  Copyright (c) 2012年 Founder. All rights reserved.
+//
+
+#ifndef Sanger_VoiceDef_h
+#define Sanger_VoiceDef_h
+
+
+
+#endif
